64f35dee17831d3d303d2a5e3f5f386f0fea93c9
shiffman <daniel@shiffman.net>
2020-03-16 13:44:07 -0400

    listen in all channels

--- src/commands/index.js
+++ src/commands/index.js
@@ -11,7 +11,7 @@ const commands = {
 
 module.exports = async (msg) => {
   // console.log(msg);
-  if (msg.guild.id === guildID && msg.channel.id === channelID) {
+  if (msg.guild.id === guildID) {
     const args = msg.content.split(' ');
     if (args.length == 0 || args[0].charAt(0) !== '!') return;
     const command = args.shift().substr(1);
