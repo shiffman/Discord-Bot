cacb7e711550754feeed1d00b3477fbbf1248578
shiffman <daniel@shiffman.net>
2020-03-19 09:07:23 -0400

    bug fixes

--- src/commands/role.js
+++ src/commands/role.js
@@ -1,11 +1,15 @@
 module.exports = async (msg, args) => {
-  if (args.length < 2) return;
-  console.log(args);
-  if (args[0] == 'add') {
+  if (args.length < 2) {
+    await msg.channel.send(`Sorry ${msg.author}, I require a command and a role name.`);
+    return;
+  };
+  if (args[0] === 'add' && args[1] === 'ima') {
     await msg.member.addRole('690032740615061565');
     await msg.channel.send(`${msg.author} The role ima has been added.`);
-  } else if (args[0] === 'remove') {
+  } else if (args[0] === 'remove' && args[1] === 'ima') {
     await msg.member.removeRole('690032740615061565');
     await msg.channel.send(`${msg.author} The role ima has been removed.`);
+  } else {
+    await msg.channel.send(`Sorry ${msg.author}, I don't know that command or role. Also I need some better error messages.`);
   }
 };
