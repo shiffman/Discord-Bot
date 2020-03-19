d0be62b74fdaaef8c651bf7e684b8bd9697c84bd
David Snyder <davidgraygs4@gmail.com>
2020-03-19 10:53:10 -0400

    refactored role changing function

--- src/commands/role.js
+++ src/commands/role.js
@@ -1,14 +1,26 @@
+const fs = require('fs');
+const path = require('path')
+
+const jsonPath = path.join(__dirname, '..', 'commands', 'roles.json');
+const data = fs.readFileSync(jsonPath, 'utf8');
+const roles = JSON.parse(data)
+console.log(roles)
+
 module.exports = async (msg, {args}) => {
   if (args.length < 2) {
     await msg.channel.send(`Sorry ${msg.author}, I require a command and a role name.`);
     return;
-  };
-  if (args[0] === 'add' && args[1] === 'ima') {
-    await msg.member.addRole('690032740615061565');
-    await msg.channel.send(`${msg.author} The role ima has been added.`);
-  } else if (args[0] === 'remove' && args[1] === 'ima') {
-    await msg.member.removeRole('690032740615061565');
-    await msg.channel.send(`${msg.author} The role ima has been removed.`);
+  }
+
+  const action = args.shift()
+  const role = args.join(' ').toLowerCase()
+
+  if (action === 'add' && role in roles) {
+    await msg.member.addRole(roles[role]);
+    await msg.channel.send(`${msg.author} The role ${role} has been added.`);
+  } else if (action === 'remove' && role in roles) {
+    await msg.member.removeRole(roles[role]);
+    await msg.channel.send(`${msg.author} The role ${role} has been removed.`);
   } else {
     await msg.channel.send(`Sorry ${msg.author}, I don't know that command or role. Also I need some better error messages.`);
   }
