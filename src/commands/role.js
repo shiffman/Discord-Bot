1897a641eeb62e4d253b362bfed54ee7b966e427
David Snyder <davidgraygs4@gmail.com>
2020-03-19 11:08:19 -0400

    added 'has' action to role manager

--- src/commands/role.js
+++ src/commands/role.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
 const jsonPath = path.join(__dirname, '..', 'commands', 'roles.json');
 const data = fs.readFileSync(jsonPath, 'utf8');
-const roles = JSON.parse(data)
-console.log(roles)
+const roles = JSON.parse(data);
+console.log(roles);
 
 module.exports = async (msg, {args}) => {
   if (args.length < 2) {
@@ -12,18 +12,23 @@ module.exports = async (msg, {args}) => {
     return;
   }
 
-  console.log("test")
-
-  const action = args.shift()
-  const role = args.join(' ').toLowerCase()
-
+  
+  const action = args.shift();
+  const role = args.join(' ').toLowerCase();
+  
+  console.log({action, role});
+  
   if (action === 'add' && role in roles) {
     await msg.member.addRole(roles[role]);
     await msg.channel.send(`${msg.author} The role ${role} has been added.`);
   } else if (action === 'remove' && role in roles) {
     await msg.member.removeRole(roles[role]);
     await msg.channel.send(`${msg.author} The role ${role} has been removed.`);
-  } else {
+  } else if (action === 'has' && role in roles) {
+    const hasRole = msg.author._roles.includes(role);
+    await msg.channel.send(`${msg.author} you ${hasRole ? 'do' : 'do not'} have the role ${role}`);
+  } 
+  else {
     await msg.channel.send(`Sorry ${msg.author}, I don't know that command or role. Also I need some better error messages.`);
   }
 };
