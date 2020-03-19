6ec8eaa2e45465ce72a3535964ba9d3b8bcf44bb
David Snyder <davidgraygs4@gmail.com>
2020-03-19 11:27:37 -0400

    remove guild check

--- src/commands/index.js
+++ src/commands/index.js
@@ -14,12 +14,10 @@ const commands = {
 };
 
 module.exports = async (msg) => {
-  if (msg.guild.id === guildID) {
     const args = msg.content.split(' ');
     if (args.length == 0 || args[0].charAt(0) !== '!') return;
     const command = args.shift().substr(1);
     if (Object.keys(commands).includes(command)) {
       commands[command](msg, { args, commands });
     }
-  }
 };
